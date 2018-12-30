78b25bbeec6292d97f0c3cdc00ccc5d25946e3cb
Patrice Matignon <pmatignon@hotmail.com>
2018-12-30 11:01:43 -0700

    Initial state with assets
    Downloaded from https://nostarch.com/download/escape.zip

---
+++ escape.py
@@ -0,0 +1,14 @@
+# Escape - A Python Adventure 
+# by Sean McManus / www.sean.co.uk
+# Art by Rafael Pimenta
+# Typed in by REMI MATIGNON
+
+
+###############
+## VARIABLES ##
+###############
+
+WIDTH = 800 #window size
+HEIGHT = 800
+
+#PLAYER variables
